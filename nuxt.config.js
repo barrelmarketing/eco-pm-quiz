e37604acebc335414a8ffeee3d4c4433eaafd2b7
DESKTOP-HDUTHV5\Shayne <shayne.plesanaden@gmail.com>
2022-02-18 15:59:03 -0700

    init

---
+++ nuxt.config.js
@@ -0,0 +1,65 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'eco-pm-quiz',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      // { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      // { rel: "stylesheet", type: "text/css", href: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" },
+      { rel: "stylesheet", type: "text/css", href: "https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" },
+      { rel: "stylesheet", type: "text/css", href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" },
+      { rel: "stylesheet", type: "text/css", href: "https://unpkg.com/aos@2.3.1/dist/aos.css" }
+    ]
+  },
+  script: [
+    // { src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js' },
+    { src: '@/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js' },
+    // { src: 'https://unpkg.com/aos@2.3.1/dist/aos.js' },
+  ],
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    '~/assets/style.scss',
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    {
+      src: '~plugins/aos.js',
+      ssr: false,
+    },
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: '/',
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,443 @@
+<template>
+  <div>
+    <header class="sticky-top container-fluid p-2" id="eco-quiz-header">
+      <div class="logo d-flex">
+        <img class="img-fluid" src="images/logos.png" alt="ECO Logo" />
+      </div>
+      <!-- SP: DEBUG -->
+      <i
+        class="fa fa-bug debug fa-3x text-secondary"
+        role="button"
+        @click="toggleDebug"
+        aria-hidden="true"
+      ></i>
+    </header>
+    <section
+      id="main-content"
+      class="container d-flex flex-column justify-content-center"
+      v-if="finishedQuiz == false"
+    >
+      <!-- SP: INTRO -->
+      <div class="row" v-if="beginQuiz == false">
+        <div
+          class="
+            col-xl-8 col-lg-9
+            mx-auto
+            d-flex
+            flex-column
+            align-items-center
+          "
+        >
+          <h1 class="fw-bold mb-4 w-100 h2">
+            Are you ready for Project Management Essentials?
+          </h1>
+          <h4 class="mb-4">
+            ECO Canada and Meridus Management Inc. have partnered to provide
+            skill development in critical project management concepts through
+            online learning.
+          </h4>
+          <p class="mb-4">
+            Meridus’ course, Project Management Essentials, is an interactive
+            online course that addresses fundamental skill gaps related to
+            communication and management of key project constraints including
+            scope, budget, schedule, and quality. This new online course is
+            available through ECO Canada’s website and is a practical learning
+            tool, regardless of the individual’s role in the project.
+          </p>
+          <button
+            class="
+              btn btn-success btn-lg
+              d-inline-flex
+              mx-auto
+              align-items-center align-self-center
+            "
+            @click="beginQuiz = true"
+          >
+            Begin Quiz
+            <i
+              class="fa fa-angle-double-right fa-2x ms-3"
+              aria-hidden="true"
+            ></i>
+          </button>
+        </div>
+      </div>
+      <!-- SP: QUIZ MAIN -->
+      <div class="row" id="quizMain" v-if="beginQuiz">
+        <div
+          class="col-xl-6 col-lg-9 mx-auto"
+          v-if="chosenQuestions.length > 0"
+        >
+          <h2 class="fw-bold">
+            {{ currentQuestion + 1 }} )
+            {{
+              chosenQuestions[currentQuestion]["question-text"].replaceAll(
+                "\\",
+                ""
+              )
+            }}
+          </h2>
+
+          <form :name="'answers_' + currentQuestion" class="mt-4">
+            <div
+              v-for="(answer, index) in chosenQuestions[currentQuestion]
+                .answers"
+              :key="index"
+            >
+              <div class="form-check mt-3">
+                <input
+                  class="form-check-input"
+                  :type="answerType"
+                  :id="'answer_' + currentQuestion + '_' + alphabet[index]"
+                  :value="alphabet[index]"
+                  :name="'answers_' + currentQuestion"
+                  v-model="chosenQuestions[currentQuestion].userAnswer"
+                />
+                <label
+                  class="form-check-label"
+                  :for="'answer_' + currentQuestion + '_' + alphabet[index]"
+                >
+                  {{ answer.replaceAll("\\", "") }}
+                </label>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+    <!-- SP: QUIZ RESULTS -->
+    <section
+      class="overflow-auto container-fluid py-5"
+      v-if="finishedQuiz == true"
+      id="resultsContainer"
+    >
+      <div class="row mt-5">
+        <div class="col-xl-6 col-lg-9 mx-auto h-100 mt-5">
+          <h3 class="w-100 text-center fw-bold">Your results:</h3>
+          <br />
+          <div class="mb-5 text-center">
+            <span class="h1">{{ score + "/" + chosenQuestions.length }}</span>
+            <div class="bg-light p-5 mt-5" v-if="score.length > 0">
+              The Project Management Essentials course would be a great
+              opportunity to level up your skills. To learn more,
+              <a
+                href="https://eco.ca/blog/eco-canada-launches-a-new-project-management-training-program-with-meridus-management-inc/"
+                class="text-success text-link"
+                >click here</a
+              >
+            </div>
+          </div>
+          <hr />
+        </div>
+      </div>
+      <!-- SP: REVIEW -->
+      <div class="row">
+        <div class="col-xl-6 col-lg-9 mx-auto">
+          <div
+            class="mt-5"
+            v-for="(question, index) in chosenQuestions"
+            :key="index"
+          >
+            <h4 class="w-100 fw-bold">
+              {{ index + 1 }} )
+              {{ question["question-text"].replaceAll("\\", "") }}
+            </h4>
+            <h5 class="mt-4 ms-2">Correct answer:</h5>
+
+            <div
+              class="text-success ms-5"
+              v-for="(ans, ansID) in question['correct-answer'].split(',')"
+              :key="ansID"
+            >
+              {{
+                ans.toUpperCase() + ") " + chosenAnswer(question, ans, ansID)
+              }}
+            </div>
+
+            <h5 class="mt-4 ms-2">You chose:</h5>
+            <div
+              :class="'w-100 ms-5' + isCorrect(question)"
+              v-for="(ans, ansID) in question.userAnswer"
+              :key="ans[ansID]"
+            >
+              {{
+                ans.toUpperCase() +
+                ") " +
+                question["answers"][alphabet.indexOf(ans)]
+              }}
+              <!-- {{ question.answers[question.userAnswer].replaceAll("\\", "") }} -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <pre class="debug" v-if="debug">
+      answer:{{ this.chosenQuestions[currentQuestion] }}
+    </pre>
+
+    <!-- SP: STAT BAR / FOOTER -->
+    <footer id="statbar" class="container-fluid" v-if="beginQuiz">
+      <div class="progress row mb-auto">
+        <div
+          class="progress-bar"
+          role="progressbar"
+          :style="'width:' + (currentQuestion + 1) * 20 + '%'"
+          :aria-valuenow="(currentQuestion + 1) * 20 + '%'"
+          aria-valuemin="0"
+          aria-valuemax="100"
+        >
+          {{ (currentQuestion + 1) * 20 + "%" }}
+        </div>
+      </div>
+      <div class="row">
+        <div class="col d-flex align-items-center justify-content-start">
+          <button
+            class="btn btn-success btn-lg d-inline-flex align-items-center"
+            @click="previousQuestion"
+            v-if="currentQuestion > 0"
+          >
+            <i
+              class="fa fa-angle-double-left fa-2x me-3"
+              aria-hidden="true"
+            ></i>
+            Previous Question
+          </button>
+        </div>
+        <div class="col p-0 text-center">
+          <div
+            class="
+              text-white
+              h5
+              rounded-circle
+              mx-auto
+              d-flex
+              align-items-center
+              justify-content-center
+              p-2
+            "
+            id="progressCircle"
+          >
+            {{ currentQuestion + 1 }} /
+            {{ chosenQuestions.length }}
+          </div>
+        </div>
+        <div class="col d-flex align-items-center justify-content-end">
+          <button
+            class="
+              btn btn-success btn-lg
+              my-auto
+              d-inline-flex
+              align-items-center
+            "
+            tabindex="1"
+            :disabled="isDisabled"
+            @click="nextQuestion"
+            v-if="currentQuestion < chosenQuestions.length - 1"
+          >
+            Next Question
+            <i
+              class="fa fa-angle-double-right fa-2x ms-3"
+              aria-hidden="true"
+            ></i>
+          </button>
+          <button
+            class="
+              btn btn-success btn-lg
+              ms-auto
+              d-inline-flex
+              align-items-center
+            "
+            :disabled="isDisabled"
+            @click="gradeQuiz"
+            v-if="currentQuestion == chosenQuestions.length - 1"
+          >
+            Get My Results
+            <i class="fa fa-check-circle fa-2x ms-3" aria-hidden="true"></i>
+          </button>
+        </div>
+      </div>
+    </footer>
+  </div>
+  <!-- <Tutorial /> -->
+</template>
+
+<script>
+var url =
+  "https://eco.ca/wp-content/themes/hello-theme-child-master/quiz_api.php";
+
+export default {
+  name: "IndexPage",
+  data() {
+    return {
+      questions: null,
+      fullQuestionList: [],
+      chosenQuestions: [],
+      currentQuestion: 0,
+      beginQuiz: false,
+      finishedQuiz: false,
+      score: [],
+      debug: false,
+      alphabet: [
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "o",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "u",
+        "v",
+        "w",
+        "x",
+        "y",
+        "z",
+      ],
+    };
+  },
+
+  methods: {
+    nextQuestion() {
+      if (this.currentQuestion < this.chosenQuestions.length - 1) {
+        this.currentQuestion++;
+        var clist = document.getElementsByClassName("form-check-input");
+        for (var i = 0; i < clist.length; ++i) {
+          clist[i].checked = false;
+        }
+      }
+    },
+    previousQuestion() {
+      if (this.currentQuestion > 0) {
+        this.currentQuestion--;
+        var clist = document.getElementsByClassName("form-check-input");
+        for (var i = 0; i < clist.length; ++i) {
+          clist[i].checked = false;
+        }
+      }
+    },
+    gradeQuiz() {
+      this.beginQuiz = false;
+      this.finishedQuiz = true;
+      var score = 0;
+      for (var i = 0; i < this.chosenQuestions.length; ++i) {
+        if (
+          this.chosenQuestions[i]["correct-answer"]
+            .split(",")
+            .includes(this.chosenQuestions[i].userAnswer)
+        ) {
+          score++;
+        }
+      }
+      this.score.push(score);
+      console.log(
+        "FINAL:",
+        this.chosenQuestions,
+        this.score + "/" + this.chosenQuestions.length
+      );
+    },
+    isCorrect: function (question) {
+      for (var a in question.answers) {
+        if (question["correct-answer"].includes(question.userAnswer)) {
+          return " fw-bold text-success";
+        } else {
+          return " fst-italic text-danger";
+        }
+      }
+    },
+    chosenAnswer: function (question, answer, ansID) {
+      var correctAnswer = question["correct-answer"].split(",");
+      var alphIndex = this.alphabet.indexOf(answer);
+      var chosenAns = question["answers"][ansID];
+      console.log(
+        "correct:",
+        correctAnswer,
+        "alphindex:",
+        alphIndex,
+        "answers:",
+        question["answers"],
+        "chosen:",
+        chosenAns
+      );
+      return chosenAns;
+    },
+    toggleDebug() {
+      this.debug = !this.debug;
+      console.log(this.debug);
+    },
+  },
+  computed: {
+    answerType: function () {
+      if (this.chosenQuestions[this.currentQuestion].answers.length <= 4) {
+        return "radio";
+      } else {
+        return "checkbox";
+      }
+    },
+    isDisabled: function () {
+      if (
+        this.chosenQuestions.length > 0 &&
+        this.chosenQuestions[this.currentQuestion].userAnswer.length == 0
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
+  mounted() {
+    this.$axios.get(url).then((response) => {
+      this.questions = Object.values(response.data);
+
+      for (var t in this.questions) {
+        if (this.questions[t] != null) {
+          for (var q in this.questions[t]) {
+            var question = {};
+            question["question-text"] = this.questions[t][q]["question-text"];
+            question["correct-answer"] = this.questions[t][q]["correct-answer"];
+            question.userAnswer = [];
+            question.answers = [];
+
+            for (var a in Object.keys(this.questions[t][q])) {
+              // console.log(Object.keys(this.questions[t][q])[a]);
+              if (Object.keys(this.questions[t][q])[a].includes("answer-")) {
+                var answer_name = Object.keys(this.questions[t][q])[a];
+                var answer = this.questions[t][q][answer_name];
+                // console.log("answer: " + answer);
+                if (answer !== "") {
+                  question.answers.push(answer);
+                }
+              }
+            }
+            // console.log(question);
+            this.fullQuestionList.push(question);
+          }
+        }
+      }
+
+      // SP: fill fullQuestionList with questions unless duplicate
+      while (this.chosenQuestions.length < 5) {
+        var randomNumber = Math.random();
+        var randomKey = Math.floor(randomNumber * this.fullQuestionList.length);
+        var randomQuestion = this.fullQuestionList[randomKey];
+        if (this.chosenQuestions.indexOf(randomQuestion) == -1) {
+          this.chosenQuestions.push(randomQuestion);
+        }
+      }
+      console.log(this.chosenQuestions);
+    });
+  },
+};
+</script>
+
